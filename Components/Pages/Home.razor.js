8968279cafa49a385b4ad51535d223fe987a2eea
Alex Klein <alex.klein4414@gmail.com>
2025-02-15 16:28:07 -0500

    Add Javascript to Project

---
+++ Components/Pages/Home.razor.js
@@ -0,0 +1,11 @@
+﻿function scrollToBottom(elementId){
+    console.log(elementId)
+    var element = document.getElementById(elementId);
+    if (element) {
+        element.scrollTop = element.scrollHeight;
+    }
+};
+
+function test() {
+    console.log("test")
+}
--- wwwroot/script.js
+++
@@ -1,3 +0,0 @@
-﻿window.reloadPage = () => {
-    location.reload();
-};
